4085990a2cfc65a86945d5feb2f47b9e1ae8d8e4
mj <xkxpa@korea.ac.kr>
2021-08-27 02:31:09 +0000

    fix errr

--- src/train_rev_explainer.sh
+++ src/train_rev_explainer.sh
@@ -1,7 +1,6 @@
-DATASET=$1
-DATA_DIR=$2
-MODEL_SAVE_DIR=$3
+DATA_DIR=$1
+MODEL_SAVE_DIR=$2
 
 DIR='pwd'
 
-python ./training/train.py --dataset $DATASET --data_directory $DATA_DIR --save_dir $MODEL_SAVE_DIR
+python ./training/train.py --data_directory $DATA_DIR --save_dir $MODEL_SAVE_DIR
