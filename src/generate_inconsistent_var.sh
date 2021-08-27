a954d8cadf6eb000e8a848a25ba04c40844331b9
MJ-Jang <xkxpa@korea.ac.kr>
2021-08-27 08:19:07 +0100

    fix typo

--- src/generate_inconsistent_var.sh
+++ src/generate_inconsistent_var.sh
@@ -1,5 +1,5 @@
 DATA_DIR=$1
-AVE_DIR=$2
+SAVE_DIR=$2
 
 # You may change this part as variables
 DATASET=esnli
@@ -8,7 +8,7 @@ MODEL_DIR=../model_binary/
 cmd="CUDA_VISIBLE_DEVICES=0  python ./training/inference.py  \
     --dataset=$DATASET
     --data_directory=$DATA_DIR \
-    --save_dir=$MODEL_SAVE_DIR  \
+    --save_dir=$SAVE_DIR  \
     --model_directory=$MODEL_DIR"
 echo $cmd
 eval $cmd
