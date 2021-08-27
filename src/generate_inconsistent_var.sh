ea3b27b555593ef799c94054c25d96ed67bf5cb9
MJ-Jang <xkxpa@korea.ac.kr>
2021-08-27 07:34:51 +0100

    update

--- src/generate_inconsistent_var.sh
+++ src/generate_inconsistent_var.sh
@@ -9,6 +9,6 @@ cmd="CUDA_VISIBLE_DEVICES=0  python ./training/inference.py  \
     --dataset=$DATASET
     --data_directory=$DATA_DIR \
     --save_dir=$MODEL_SAVE_DIR  \
-    --model_directory $MODEL_DIR"
+    --model_directory=$MODEL_DIR"
 echo $cmd
 eval $cmd
